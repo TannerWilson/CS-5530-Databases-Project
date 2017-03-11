1857fab42fad07d4d7410680983ef5449f1d87f0
greybonez <u0866499@utah.edu>
2017-03-10 19:07:11 -0700

    first commit

---
+++ phase_2_java/src/com/gradeycullins/Connector.java
@@ -0,0 +1,38 @@
+package com.gradeycullins;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * Created by Gradey Cullins on 3/10/17.
+ */
+public class Connector {
+    public Connection con;
+    public Statement stmt;
+    public Connector() throws Exception {
+        try{
+//            String userName = "5530user";
+//            String password = "5530@16";
+//            String url = "jdbc:mysql://georgia.eng.utah.edu/cs5530db?autoReconnect=true&useSSL=false";
+            String userName = "5530u58";
+            String password = "o3tve4iq";
+            String url = "jdbc:mysql://georgia.eng.utah.edu/5530db58?autoReconnect=true&useSSL=false";
+            Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+            con = DriverManager.getConnection (url, userName, password);
+
+            //DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
+            //stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            stmt = con.createStatement();
+            //stmt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        } catch(Exception e) {
+            System.err.println("Unable to open mysql jdbc connection. The error is as follows,\n");
+            System.err.println(e.getMessage());
+            throw(e);
+        }
+    }
+
+    public void closeConnection() throws Exception{
+        con.close();
+    }
+}
---
+++ phase_2_java/src/com/gradeycullins/Main.java
@@ -0,0 +1,12 @@
+package com.gradeycullins;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Connector conn = new Connector();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
